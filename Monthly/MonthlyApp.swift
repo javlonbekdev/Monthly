265f933441b2310429e24050ce499c426fd5b991
Javlonbek <javlonbekdev@gmail.com>
2023-01-26 16:21:40 +0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Javlonbek on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
